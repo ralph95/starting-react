90d27c4e4b7f1fef556c1aaf3b8fc9aaf990e35f
ralph95 <ralphbaylon95@gmail.com>
2024-08-04 13:28:50 +0800

    Added component libraries from material ui. Using command: yarn add @material-ui/core

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import React from "react";
 import "./App.css";
 import PropTypes from "prop-types";
 import styled from "@emotion/styled";
+import { Button } from "@material-ui/core";
 
 const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
     <td>{pokemon.name.english}</td>
     <td>{pokemon.type.join(",")}</td>
     <td>
-      <button onClick={() => onSelect(pokemon)}>Select</button>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => onSelect(pokemon)}
+      >
+        Select
+      </Button>
     </td>
   </tr>
 );
