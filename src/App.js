530f00dc39e4cfff0de293abe9471345e8098e75
ralph95 <ralphbaylon95@gmail.com>
2024-08-04 22:03:15 +0800

    Revert Changes for the context tutorial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Button } from "@material-ui/core";
 const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
     <td>{pokemon.name.english}</td>
-    <td>{pokemon.type.join(", ")}</td>
+    <td>{pokemon.type.join(",")}</td>
     <td>
       <Button
         variant="contained"
@@ -20,7 +20,7 @@ const PokemonRow = ({ pokemon, onSelect }) => (
   </tr>
 );
 
-// Ensure that the data received by the component has the correct set of data values
+//Ensure that the data receive to the component has correct set of data values. Creating a restriction
 PokemonRow.propTypes = {
   pokemon: PropTypes.shape({
     name: PropTypes.shape({
@@ -30,24 +30,21 @@ PokemonRow.propTypes = {
   }).isRequired,
   onSelect: PropTypes.func,
 };
-
 const PokemonInfo = ({ name, base }) => (
   <div>
     <h1>{name.english}</h1>
     <table>
-      <tbody>
-        {Object.keys(base).map((key) => (
-          <tr key={key}>
-            <td>{key}</td>
-            <td>{base[key]}</td>
-          </tr>
-        ))}
-      </tbody>
+      {Object.keys(base).map((key) => (
+        <tr key={key}>
+          <td>{key}</td>
+          <td>{base[key]}</td>
+        </tr>
+      ))}
     </table>
   </div>
 );
 
-// Ensure that the data received are from PokemonInfo data
+//Ensure that the data received are from PokemonInfo data
 PokemonInfo.propTypes = {
   name: PropTypes.shape({
     english: PropTypes.string.isRequired,
@@ -62,12 +59,12 @@ PokemonInfo.propTypes = {
   }),
 };
 
-// Creation of emotion styled CSS
+//Creation of emotion styled css
 const Title = styled.h1`
   text-align: center;
 `;
 
-const TwoColumnLayout = styled.div`
+const TwoColunmLayout = styled.div`
   display: grid;
   grid-template-columns: 70% 30%;
   grid-column-gap: 1rem;
@@ -85,66 +82,51 @@ const Input = styled.input`
   padding: 0.2rem;
 `;
 
-// Class Base Components extends App() and overwrite super
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      filter: "",
-      pokemon: [],
-      selectedItem: null,
-    };
-  }
-
-  componentDidMount() {
+//Starting the Application
+function App() {
+  const [filter, filterSet] = React.useState("");
+  const [pokemon, pokemonSet] = React.useState([]);
+  const [selectedItem, selectedItemSet] = React.useState(null);
+  React.useEffect(() => {
     fetch("http://localhost:3000/starting-react/pokemon.json")
       .then((resp) => resp.json())
-      .then((pokemon) => this.setState({ pokemon }));
-  }
+      .then((data) => pokemonSet(data));
+  }, []);
 
-  render() {
-    return (
-      <Container>
-        <Title className="title">Pokemon Search</Title>
-        <Input
-          value={this.state.filter}
-          onChange={(evt) => this.setState({ filter: evt.target.value })}
-        />
-        <TwoColumnLayout>
-          <div>
-            <table width="100%">
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Type</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.pokemon
-                  .filter((pokemon) =>
-                    pokemon.name.english
-                      .toLowerCase()
-                      .includes(this.state.filter.toLowerCase())
-                  )
-                  .map((pokemon) => (
-                    <PokemonRow
-                      pokemon={pokemon}
-                      key={pokemon.id}
-                      onSelect={(pokemon) =>
-                        this.setState({ selectedItem: pokemon })
-                      }
-                    />
-                  ))}
-              </tbody>
-            </table>
-          </div>
-          {this.state.selectedItem && (
-            <PokemonInfo {...this.state.selectedItem} />
-          )}
-        </TwoColumnLayout>
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <Title className="title"> Pokemon Search</Title>
+      <Input value={filter} onChange={(evt) => filterSet(evt.target.value)} />
+      <TwoColunmLayout>
+        <div>
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) =>
+                  pokemon.name.english
+                    .toLowerCase()
+                    .includes(filter.toLowerCase())
+                )
+                .map((pokemon) => (
+                  <PokemonRow
+                    pokemon={pokemon}
+                    key={[pokemon.id]}
+                    onSelect={(pokemon) => selectedItemSet(pokemon)}
+                  />
+                ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedItem && <PokemonInfo {...selectedItem} />}
+      </TwoColunmLayout>
+    </Container>
+  );
 }
 
 export default App;
